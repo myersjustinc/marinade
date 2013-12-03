ad01885206f42d44176816afa71a6955f837cc8d
Justin Myers <justin@justinmyers.net>
2013-12-02 21:39:14 -0500

    Made script a bit more useful for testing

--- populate/test_fill.py
+++ populate/test_fill.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import io
 import os.path
+import sys
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from reportlab.lib.units import inch
@@ -10,36 +11,41 @@ from reportlab.pdfgen import canvas
 FORMS_DIR = os.path.join(os.path.dirname(__file__), 'blank_forms')
 
 
-overlay_file = io.BytesIO()
-overlay = canvas.Canvas(overlay_file, pagesize=(8.5 * inch, 11 * inch,))
+def fill_out_front(last_name_string):
+    overlay_file = io.BytesIO()
+    overlay = canvas.Canvas(overlay_file, pagesize=(8.5 * inch, 11 * inch,))
 
-last_name = overlay.beginText()
-last_name.setTextOrigin(0.53 * inch, (11 - 1.55) * inch)
-last_name.setFont('Helvetica', 12)
-last_name.textOut('Myers')
+    last_name = overlay.beginText()
+    last_name.setTextOrigin(0.53 * inch, (11 - 1.55) * inch)
+    last_name.setFont('Helvetica', 12)
+    last_name.textOut(last_name_string)
 
-overlay.drawText(last_name)
-overlay.showPage()
-overlay.save()
+    overlay.drawText(last_name)
+    overlay.showPage()
+    overlay.save()
 
-overlay_file.seek(0)
-overlay = PdfFileReader(overlay_file)
+    overlay_file.seek(0)
+    overlay = PdfFileReader(overlay_file)
 
-form_605_file = io.open(os.path.join(FORMS_DIR, 'ncvec605.pdf'), 'rb')
-form_605 = PdfFileReader(form_605_file)
+    form_605_file = io.open(os.path.join(FORMS_DIR, 'ncvec605.pdf'), 'rb')
+    form_605 = PdfFileReader(form_605_file)
 
-completed_file = io.open(os.path.join(FORMS_DIR, 'completed.pdf'), 'wb')
-completed = PdfFileWriter()
+    completed_file = io.open(os.path.join(FORMS_DIR, 'completed.pdf'), 'wb')
+    completed = PdfFileWriter()
 
-for page_num in (0,):
-  form_605_page = form_605.getPage(page_num)
-  overlay_page = overlay.getPage(page_num)
+    for page_num in (0,):
+      form_605_page = form_605.getPage(page_num)
+      overlay_page = overlay.getPage(page_num)
 
-  form_605_page.mergePage(overlay_page)
-  completed.addPage(form_605_page)
+      form_605_page.mergePage(overlay_page)
+      completed.addPage(form_605_page)
 
-completed.write(completed_file)
+    completed.write(completed_file)
 
-completed_file.close()
-overlay_file.close()
-form_605_file.close()
+    completed_file.close()
+    overlay_file.close()
+    form_605_file.close()
+
+
+if __name__ == '__main__':
+    fill_out_front(sys.argv[1])
