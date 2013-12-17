12bec341ba8d9c0ba58c3a0e2f7c7a3cafe24bfb
Justin Myers <justin@justinmyers.net>
2013-12-16 21:33:35 -0500

    Refactored for extensibility
    When there are docs, I'll add this to them, but:
    
    * `populate.forms` should have a module for each available form.
    
    * Each form name (module name within `populate.forms`) should correspond
      to a PDF of a blank form within `populate/blank_forms`.
    
    * Each form's module should contain a function `fill_out_form` that
      accepts the following positional arguments:
    
      * The blank form's path.
      * The settings dict with the field boundaries.
      * The contents dict to use when populating the form.

---
+++ populate/forms/ncvec_605.py
@@ -0,0 +1,51 @@
+import os.path
+from shutil import copyfileobj
+
+from populate.pdf_utils import Bounds, fill_out_form, FORMS_DIR
+
+
+FORM_605_FIELDS = {
+    'last_name': Bounds(n=9.44, e=2.54, s=9.44, w=0.54),
+    'suffix': Bounds(n=9.44, e=3.2, s=9.44, w=2.58),
+    'first_name': Bounds(n=9.44, e=4.97, s=9.44, w=3.3),
+    'middle_initial': Bounds(n=9.44, e=5.39, s=9.44, w=5.1),
+    'current_call_sign': Bounds(n=9.44, e=7.94, s=9.44, w=5.53),
+    'mailing_address': Bounds(n=9.05, e=5.39, s=8.05, w=0.54),
+    'ssn_or_frn': Bounds(n=9.05, e=7.94, s=8.05, w=5.53),
+    'city': Bounds(n=8.73, e=2.54, s=8.73, w=0.54),
+    'state': Bounds(n=8.73, e=3.81, s=8.73, w=3.3),
+    'zip': Bounds(n=8.73, e=5.39, s=8.73, w=3.94),
+    'email': Bounds(n=8.73, e=7.94, s=8.73, w=5.53),
+    'phone': Bounds(n=8.32, e=2.54, s=8.32, w=0.54),
+    'fax': Bounds(n=8.32, e=5.39, s=8.32, w=3.3),
+    'club_name': Bounds(n=8.32, e=7.94, s=8.32, w=5.53),
+    'club_call_sign': Bounds(n=7.93, e=7.94, s=7.93, w=5.53),
+    'pending_app_purpose': Bounds(n=5.79, e=5.75, s=5.79, w=3.44),
+    'former_last_name': Bounds(n=6.35, e=2.86, s=6.35, w=1.87),
+    'former_first_name': Bounds(n=6.35, e=3.59, s=6.35, w=2.97),
+    'former_middle_initial': Bounds(n=6.35, e=4.11, s=6.35, w=3.65),
+    'exam_date': Bounds(n=3.39, e=7.94, s=3.39, w=5.38),
+    'exam_location': Bounds(n=3.00, e=7.94, s=3.00, w=5.38),
+    'vec': Bounds(n=2.65, e=7.94, s=2.65, w=5.38),
+    'applicant_type': {
+        'individual': Bounds(n=8.17, e=1.51, s=7.99, w=1.33),
+        'amateur_club': Bounds(n=8.17, e=2.49, s=7.99, w=2.31),
+        'military_recreation': Bounds(n=8.17, e=3.48, s=7.99, w=3.30),
+        'races': Bounds(n=8.17, e=4.54, s=7.99, w=4.36),
+    },
+    'application_type': {
+        'new_examination': Bounds(n=7.58, e=0.75, s=7.34, w=0.57),
+        'upgrade_examination': Bounds(n=7.98, e=0.75, s=7.74, w=0.57),
+        'name_change': Bounds(n=6.94, e=0.75, s=6.70, w=0.57),
+        'address_change': Bounds(n=7.58, e=4.74, s=7.34, w=4.56),
+        'call_sign_change': Bounds(n=7.26, e=4.74, s=7.02, w=4.56),
+        'license_renewal': Bounds(n=6.61, e=4.74, s=6.37, w=4.56),
+    },
+}
+
+
+def fill_out(contents):
+    return fill_out_form(
+        os.path.join(FORMS_DIR, 'ncvec_605.pdf'),
+        FORM_605_FIELDS,
+        contents)
---
+++ populate/pdf_utils.py
@@ -0,0 +1,106 @@
+from collections import namedtuple
+import io
+import os.path
+import sys
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from reportlab.lib.colors import black
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
+
+
+Bounds = namedtuple('Bounds', ('n', 'e', 's', 'w',))
+
+
+FORMS_DIR = os.path.join(os.path.dirname(__file__), 'blank_forms')
+
+
+def fill_field(pdf_canvas, bounds, contents):
+    text_obj = pdf_canvas.beginText()
+    text_obj.setTextOrigin(bounds.w * inch, bounds.n * inch)
+    text_obj.setFont('Helvetica', 12)
+    text_obj.textOut(contents)
+
+    # If we're too wide, scale the font size in order to make everything fit.
+    current_x = text_obj.getX() / inch
+    if current_x > bounds.e:
+        scale_factor = (bounds.e - bounds.w) / (current_x - bounds.w)
+
+        text_obj = pdf_canvas.beginText()
+        text_obj.setTextOrigin(bounds.w * inch, bounds.n * inch)
+        text_obj.setFont('Helvetica', 12 * scale_factor)
+        text_obj.textOut(contents)
+
+    pdf_canvas.drawText(text_obj)
+
+
+def add_x_mark(pdf_canvas, bounds):
+    path_obj = pdf_canvas.beginPath()
+
+    pdf_canvas.setStrokeColor(black)
+    pdf_canvas.setLineWidth(3)
+
+    path_obj.moveTo(bounds.w * inch, bounds.n * inch)
+    path_obj.lineTo(bounds.e * inch, bounds.s * inch)
+    path_obj.moveTo(bounds.e * inch, bounds.n * inch)
+    path_obj.lineTo(bounds.w * inch, bounds.s * inch)
+
+    pdf_canvas.drawPath(path_obj)
+
+
+def fill_out_form(blank_pdf_path, settings, contents):
+    overlay_file = io.BytesIO()
+    overlay = canvas.Canvas(overlay_file, pagesize=(8.5 * inch, 11 * inch,))
+
+    for field_name, field_contents in contents.iteritems():
+        field_spec = settings.get(field_name, None)
+        if field_spec is None:
+            # Ignore the nonexistent field.
+            continue
+
+        if isinstance(field_spec, Bounds):
+            fill_field(overlay, field_spec, field_contents)
+        else:
+            # Assume it's a checkbox specification.
+            possible_values = field_spec.keys()
+            values_to_check = []
+
+            if isinstance(field_contents, basestring):
+                # Fill in the specified checkbox.
+                if field_contents in possible_values:
+                    values_to_check.append(field_contents)
+            else:
+                values_to_check.extend([i for i in field_contents
+                    if i in possible_values])
+
+            # Fill in each checkbox required.
+            for checkbox_value in values_to_check:
+                add_x_mark(overlay, field_spec[checkbox_value])
+
+    overlay.showPage()
+    overlay.save()
+
+    overlay_file.seek(0)
+    overlay = PdfFileReader(overlay_file)
+
+    blank_pdf_file = io.open(blank_pdf_path, 'rb')
+    blank_pdf = PdfFileReader(blank_pdf_file)
+
+    completed_file = io.BytesIO()
+    completed = PdfFileWriter()
+
+    for page_num in (0,):
+      blank_pdf_page = blank_pdf.getPage(page_num)
+      overlay_page = overlay.getPage(page_num)
+
+      blank_pdf_page.mergePage(overlay_page)
+      completed.addPage(blank_pdf_page)
+
+    completed.addPage(blank_pdf.getPage(1))
+    completed.write(completed_file)
+
+    overlay_file.close()
+    blank_pdf_file.close()
+
+    completed_file.seek(0)
+    return completed_file
