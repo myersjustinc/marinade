b7dd6d290845258948414b17277779be736068b3
Justin Myers <justin@justinmyers.net>
2014-02-04 17:59:04 -0500

    Added view and URLconf entry for /

--- prereg/urls.py
+++ prereg/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 import exams.urls
+from prereg.views import HomeView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^exams/', include(exams.urls)),
 )
---
+++ prereg/views.py
@@ -0,0 +1,5 @@
+from vanilla import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
