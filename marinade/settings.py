f3c792d082ba3fabb7797a2b18f3dbaa1f97504f
Justin Myers <justin@justinmyers.net>
2014-02-16 15:09:25 -0500

    Added ALLOWED_HOSTS defaults

--- marinade/settings.py
+++ marinade/settings.py
@@ -10,7 +10,13 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = os.environ.get('DEBUG', False) not in ('False', False,)
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+_ALLOWED_HOSTS = (
+    'localhost',
+    '.local',
+    '127.0.0.1',
+    '::1',
+)
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', _ALLOWED_HOSTS)
 
 INSTALLED_APPS = (
     'django.contrib.admin',
